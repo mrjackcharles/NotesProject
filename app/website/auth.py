6ec0eb524454c1371754fc7f4797112b4a0d8dae
Jack C <92857396+mrjackcharles@users.noreply.github.com>
2021-12-16 22:54:49 +0000

    Update auth.py

--- app/website/auth.py
+++ app/website/auth.py
@@ -58,7 +58,6 @@ def sign_up():
             new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
-            login_user(user, remember=True)
             flash('Account created successfully!', 'success')
             return redirect(url_for('views.home'))
 
